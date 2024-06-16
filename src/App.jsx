8394ab3265ab594da7da47cc4eeb10cb39a06e92
Sumeru Das <sumerudas2019@gmail.com>
2024-06-16 20:02:42 +0530

    Add styling to BookSearch Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import BookSearch from "./component/BookSearch";
 const App = () => {
   return (
     <div className="App">
-      <header className="bg-blue-500 text-white p-4 text-center">
-        <h1 className="text-2xl">Book Search App</h1>
+      <header className="bg-[#6B08F0] text-white p-4 flex flex-col justify-center items-center h-[166px] gap-4 leading-6">
+        <h1 className="text-[10vw] sm:text-7xl tracking-tighter">Book Search App</h1>
+        <p className="text-[3vw] sm:text-2xl sm:font-normal font-semibold">including the young adult dystopian novel Departures and the</p>
       </header>
       <main className="p-4">
         <BookSearch />
